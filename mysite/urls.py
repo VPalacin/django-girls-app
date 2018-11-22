9b01bc89edec6de13c4ea9823afebc762ff6265e
vpalacin <vpalacin@deloitte.com>
2018-11-22 15:21:46 -0300

    template added

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import url, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
